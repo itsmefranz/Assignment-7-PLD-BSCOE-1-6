e51d81390e311277b1bdf6d86b6ea82359ef0de0
Daniella Francine Velasquez <daniellafrancine.velasquez@gmail.com>
2021-11-29 01:42:18 +0800

    major changes: added loop for is statements that would display if invalid password, still having problems

--- program2.py
+++ program2.py
@@ -1,7 +1,25 @@
 def validate_pass():
-    while True:
-        password= input("Please enter a password: ")
-        if len(password) < 15:
-            print("Unacceptable password. Please make sure your password has at least 15 letters or more.")
+    upper, digit, special= 0, 0, 0
+    password= input("Please enter a password here: ")
+    if len(password) >= 15 :
+        for i in password:
+            for word in password.split():
+                if any([x.isupper() for x in password]):
+                    upper += 1
+                if any([x.isdigit() for x in password]):
+                    digit += 1
+                if i == '~' or i == '`' or i == '!' or i == '@' or i == '#' or i == '$' or i == '%' or i == '^' or i == '&' or i == '*' or i == '(' or i == ')' or i == '_' or i == '–' or i == '+' or i == '=' or i == '{' or i == '[' or i == '}' or i == ']' or i == '|' or i == '/' or i == ':' or i == ';' or i == '“' or i == '‘' or i == '<' or i == ',' or i == '>' or i == '.' or i == '?':
+                    special += 1
+    else:
+       if upper <1:
+           print("Invalid. Your password should include at least one uppercase letter.")
+           if digit <1:
+               print("Invalid. Your password should include a number in it.")
+               if special < 1:
+                   print("Invalid. Your password should include at least one special character.")
+                   if len(password) < 15:
+                       print("Invalid. Your password should at least include 15 characters.")
+    if (upper >= 1 and digit >= 1 and special >= 1):
+        print("Valid Password")
 
 validate_pass()
