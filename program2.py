ab5663b6e17e78e494184a87803d72f6561d6b26
Daniella Francine Velasquez <daniellafrancine.velasquez@gmail.com>
2021-11-29 01:46:40 +0800

    update2.0: STILL NOT WORKING it wont display the desired output i am about to give up

--- program2.py
+++ program2.py
@@ -1,21 +1,20 @@
 def validate_pass():
     upper, digit, special= 0, 0, 0
     password= input("Please enter a password here: ")
-    if len(password) >= 15 :
-        for i in password:
-            for word in password.split():
-                if any([x.isupper() for x in password]):
-                    upper += 1
-                if any([x.isdigit() for x in password]):
-                    digit += 1
-                if i == '~' or i == '`' or i == '!' or i == '@' or i == '#' or i == '$' or i == '%' or i == '^' or i == '&' or i == '*' or i == '(' or i == ')' or i == '_' or i == '–' or i == '+' or i == '=' or i == '{' or i == '[' or i == '}' or i == ']' or i == '|' or i == '/' or i == ':' or i == ';' or i == '“' or i == '‘' or i == '<' or i == ',' or i == '>' or i == '.' or i == '?':
-                    special += 1
+    for i in password:
+        for word in password.split():
+            if word.any([x.isupper() for x in password]):
+                upper += 1
+            if word.any([x.isdigit() for x in password]):
+                digit += 1
+            if i == '~' or i == '`' or i == '!' or i == '@' or i == '#' or i == '$' or i == '%' or i == '^' or i == '&' or i == '*' or i == '(' or i == ')' or i == '_' or i == '–' or i == '+' or i == '=' or i == '{' or i == '[' or i == '}' or i == ']' or i == '|' or i == '/' or i == ':' or i == ';' or i == '“' or i == '‘' or i == '<' or i == ',' or i == '>' or i == '.' or i == '?':
+                special += 1
     else:
-       if upper == 0:
+       if upper <1:
            print("Invalid. Your password should include at least one uppercase letter.")
-           if digit == 0:
+           if digit <1:
                print("Invalid. Your password should include a number in it.")
-               if special == 0:
+               if special < 1:
                    print("Invalid. Your password should include at least one special character.")
                    if len(password) < 15:
                        print("Invalid. Your password should at least include 15 characters.")
